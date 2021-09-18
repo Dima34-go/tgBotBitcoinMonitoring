4e6c33744d0040f019e9cbdf79b93ebfcf8de538
Dima34-go <85898117+Dima34-go@users.noreply.github.com>
2021-09-18 04:40:07 +0300

    first commit

---
+++ errors.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+//места возможного возникновения ошибок в коде с парсингом сайта
+var (
+	placeCallbackQuery ="CallbackQuery message"
+	placeMessageCommand = "MessageCommand message"
+	placeMessageNotCommand = "MessageNotCommand message"
+	placeSendMessageAboutBitcoin = "SendMessageAboutBitcoin message"
+)
+func errorsMessage(place string,err error){
+	if err!=nil{
+		fmt.Println(place,": ",err)
+	}
+}
+//места возможного возникновения ошибок в коде с базой данных
+var (
+	InfoBitcoinDB ="problem in work InfoBitcoinDB "
+	ChatIdCostDB ="problem in work ChatIdCostDB "
+	ChatIdChangeCostDB ="problem in work ChatIdChangeCostDB "
+)
+//операции с базой данных
+var (
+	changeInfo = "changeInfo function not work"
+	giveInfo = "giveInfo function not work"
+	addInfo = "addInfo function not work"
+	deleteInfo= "deleteInfo function not work"
+)
+func errorsWorkDB(place string,operation string ,err error){
+	if err!=nil{
+		fmt.Println(place,operation,": ",err)
+	}
+}
---
+++ pars.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"errors"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/geziyor/geziyor"
+	"github.com/geziyor/geziyor/client"
+	"github.com/geziyor/geziyor/export"
+	"strconv"
+	"strings"
+)
+type infoBitcoin struct {
+	Cost float64
+	changeCostPr float64
+	changeCostUSD float64
+	isIncrease bool
+}
+//Парсится страница https://ru.investing.com/crypto/bitcoin
+//получение ссылок на новости
+func parsNews() ([]string,error){
+	prefix:="https://ru.investing.com"
+	//Количество считываемых новостей
+	quatityNews:=3
+	var allNews []string
+	var err error
+	i:=0
+	geziyor.NewGeziyor(&geziyor.Options{
+		StartURLs: []string{"https://ru.investing.com/crypto/bitcoin/news"},
+		ParseFunc: func(g *geziyor.Geziyor, r *client.Response) {
+			r.HTMLDoc.Find("div.js-articles-wrapper.largeTitle.js-news-items").Find("a.img").Each( func( size int , s *goquery.Selection) {
+				if i<quatityNews{
+					url,ok:=s.Attr("href")
+					if !ok{
+						err=errors.New("проблема с парсингом новостей")
+					}
+					if url[0:4]!="http"{
+						url=prefix+url
+					}
+					allNews=append(allNews,url)
+					i++
+				}
+			})
+		},
+	}).Start()
+	return allNews,err
+}
+//получение ссылок на аналитику
+func parsAnalysis() ([]string,error){
+	prefix:="https://ru.investing.com"
+	//Количество считываемых аналитик
+	quatityAnalysis:=3
+	var allAnalysis []string
+	var err error
+	i:=0
+	geziyor.NewGeziyor(&geziyor.Options{
+		StartURLs: []string{"https://ru.investing.com/crypto/bitcoin/analysis"},
+		ParseFunc: func(g *geziyor.Geziyor, r *client.Response) {
+			r.HTMLDoc.Find("div.js-content-wrapper").Find("a.img").Each( func( size int , s *goquery.Selection) {
+				if i<quatityAnalysis{
+					url,ok:=s.Attr("href")
+					if url[0:4]!="http"{
+						url=prefix+url
+					}
+					if !ok{
+						err=errors.New("проблема с парсингом аналитики")
+					}
+					allAnalysis=append(allAnalysis,url)
+					i++
+				}
+			})
+		},
+	}).Start()
+	return allAnalysis,err
+}
+//получение всей информации о цене с сайта, а именно цены,роста цены, роста в процентах
+func parsAllInfoAboutBitcoin() (infoBitcoin,error)  {
+	var costBitcoinStr,changeCostPrStr,changeCostUSDStr string
+	var isIncrease  bool
+	geziyor.NewGeziyor(&geziyor.Options{
+		StartURLs: []string{"https://ru.investing.com/crypto/bitcoin"},
+		ParseFunc: func(g *geziyor.Geziyor, r *client.Response) {
+			// два варианта развития событий, либо стоимость поднимается, либо опускается
+			//случай падения цены
+			costBitcoinStr=r.HTMLDoc.Find("span#last_last.pid-1057391-last").Text()
+			changeCostPrStr=r.HTMLDoc.Find("span.arial_20.pid-1057391-pcp.parentheses.redFont").Text()
+			changeCostUSDStr=r.HTMLDoc.Find("span.arial_20.pid-1057391-pc.redFont").Text()
+			if changeCostPrStr==""{//случай подорожания
+				changeCostPrStr=r.HTMLDoc.Find("span.arial_20.pid-1057391-pcp.parentheses.greenFont").Text()
+				changeCostUSDStr=r.HTMLDoc.Find("span.arial_20.pid-1057391-pc.greenFont").Text()
+				isIncrease=true
+			}
+		},
+		Exporters: []export.Exporter{&export.JSON{}},
+	}).Start()
+	//получены необработанные данные, нужно превратить их в цифры
+	//обработка стоимости биткойна
+	costBitcoinStr=strings.ReplaceAll(costBitcoinStr,".","")
+	costBitcoinStr=strings.ReplaceAll(costBitcoinStr,",",".")
+	costBitcoin,err:=strconv.ParseFloat(costBitcoinStr,64)
+	if err!=nil{
+		return infoBitcoin{},err
+	}
+	//обработка изменения цены
+	changeCostUSDStr=strings.ReplaceAll(changeCostUSDStr,"+","")
+	//changeCostUSDStr=strings.ReplaceAll(changeCostUSDStr,"-","")
+	changeCostUSDStr=strings.ReplaceAll(changeCostUSDStr,".","")
+	changeCostUSDStr=strings.ReplaceAll(changeCostUSDStr,",",".")
+	changeCostUSD,err:=strconv.ParseFloat(changeCostUSDStr,64)
+	if err!=nil{
+		return infoBitcoin{},err
+	}
+	//обработка изменения цены в процентах
+	changeCostPrStr=strings.ReplaceAll(changeCostPrStr,"+","")
+	changeCostPrStr=strings.ReplaceAll(changeCostPrStr,"%","")
+	//changeCostUSDStr=strings.ReplaceAll(changeCostUSDStr,"-","")
+	changeCostPrStr=strings.ReplaceAll(changeCostPrStr,".","")
+	changeCostPrStr=strings.ReplaceAll(changeCostPrStr,",",".")
+	changeCostPr,err:=strconv.ParseFloat(changeCostPrStr,64)
+	if err!=nil{
+		return infoBitcoin{},err
+	}
+	return infoBitcoin{costBitcoin,changeCostPr,changeCostUSD,isIncrease},nil
+}
